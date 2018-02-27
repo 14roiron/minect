b669324b6bfe5b830f993263d75f5f380e641f33
federicober <federico.ober@hotmail.com>
2018-02-27 11:40:35 +0100

    test suite v1

--- Assets/Easy_trajectory.cs
+++ Assets/Easy_trajectory.cs
@@ -3,6 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Easy_trajectory : MonoBehaviour {
+	public enum trajectoryTypes {
+		None,
+		Sin,
+		Cos,
+		Triangle
+	}
+
+	public trajectoryTypes xTrajectory;
+	public float xFreq = 1;
+	public trajectoryTypes yTrajectory;
+	public float yFreq = 1;
+	public trajectoryTypes zTrajectory;
+	public float zFreq = 1;
 
 	// Use this for initialization
 	void Start () {
@@ -11,6 +24,27 @@ public class Easy_trajectory : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		gameObject.transform.position = new Vector3 (Mathf.Sin (2 * Time.time), Mathf.Sin (3 * Time.time), 0);
+		float x, y, z = 0;
+		x = getTrajectory (xTrajectory, xFreq);
+		y = getTrajectory (yTrajectory, yFreq);
+		z = getTrajectory (zTrajectory, zFreq);
+		gameObject.transform.position = new Vector3 (x, y, z);
+	}
+
+	float getTrajectory(trajectoryTypes type, float freq) {
+		if (type == trajectoryTypes.Cos) {
+			return Mathf.Cos (2 * freq * Mathf.PI * Time.time);
+		}
+		if (type == trajectoryTypes.Sin) {
+			return Mathf.Sin (2 * freq * Mathf.PI * Time.time);
+		} 
+		if (type == trajectoryTypes.Triangle) {
+			return Mathf.Cos (2 * freq * Mathf.PI * Time.time);
+		} 
+		if (type == trajectoryTypes.None) {
+			return 0;
+		} else {
+			return 0;
+		}
 	}
 }
